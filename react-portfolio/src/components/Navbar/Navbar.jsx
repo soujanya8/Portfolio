0cb318cb7b415f290221ee016232fd701e90a09c
soujanya8 <soujanyagodugu8@gmail.com>
2025-04-21 22:55:58 -0700

    Navbar2

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,26 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import styles from "./Navbar.module.css";
-import {getImageUrl} from "../../utils";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return <nav className={styles.navbar}>
-        <a className = {styles.title} href = "/">Portfolio</a>
-        <div className= {styles.menu}>
-            <img className= {styles.menuBtn} src ={getImageUrl("nav/menuIcon.png")} />
-            <ul className= {styles.menuItems}>  
+        <a className={styles.title} href="/">Portfolio</a>
+        <div className={styles.menu}>
+            <img className={styles.menuBtn} src={menuOpen ? getImageUrl("nav/menuIcon.png") : getImageUrl("nav/closeIcon.png")} alt="menu-button" onClick={() => setMenuOpen(!menuOpen)} />
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
                 <li>
-                    <a href = "#about">About</a>
+                    <a href="#about">About</a>
                 </li>
                 <li>
-                    <a href = "#experience">Experience</a>
+                    <a href="#experience">Experience</a>
                 </li>
                 <li>
-                    <a href = "#projects">Projects</a>
+                    <a href="#projects">Projects</a>
                 </li>
                 <li>
-                    <a href = "#contact">Contact</a>
+                    <a href="#contact">Contact</a>
                 </li>
             </ul>
         </div>
